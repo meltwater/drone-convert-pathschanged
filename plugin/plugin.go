d6f721ad6b2ae0552783f066efc81bd4f4365125
Jim Sheldon <jim.sheldon@meltwater.com>
2020-06-18 16:57:05 -0400

    starting to add tests

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -14,7 +14,6 @@ import (
 
 	"github.com/buildkite/yaml"
 	"github.com/sirupsen/logrus"
-	
 )
 
 type (
--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -4,8 +4,63 @@
 
 package plugin
 
-import "testing"
+import (
+	"context"
+	"io/ioutil"
+	"testing"
+
+	"github.com/drone/drone-go/drone"
+	"github.com/drone/drone-go/plugin/converter"
+)
+
+// empty context
+var noContext = context.Background()
 
 func TestPlugin(t *testing.T) {
-	t.Skip()
+	req := &converter.Request{
+		Build: drone.Build{
+			After: "3d21ec53a331a6f037a91c368710b99387d012c1",
+		},
+		Repo: drone.Repo{
+			Slug:   "somewhere/over-the-rainbow",
+			Config: ".drone.yml",
+		},
+	}
+
+	plugin := New("invalidtoken")
+
+	config, err := plugin.Convert(noContext, req)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if config.Data != "" {
+		t.Error("An empty pipeline returned data")
+	}
+	before, err := ioutil.ReadFile("testdata/single_pipeline.yml")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	after, err := ioutil.ReadFile("testdata/single_pipeline.yml.golden")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	req.Repo.Config = "single_pipeline.yml"
+	req.Config.Data = string(before)
+	config, err = plugin.Convert(noContext, req)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if config.Data == "" {
+		t.Error("Want non-empty configuration")
+		return
+	}
+	if want, got := config.Data, string(after); want != got {
+		t.Errorf("Want %q got %q", want, got)
+	}
 }
