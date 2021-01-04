a24f7c78abb542e77860e73bb1842e0321059bc5
Jim Sheldon <jim.sheldon@meltwater.com>
2021-01-04 09:00:40 -0500

    adding arm64 support (#42)
    * adding arm64 support

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,3 +11,4 @@ set -x
 
 # linux
 GOOS=linux GOARCH=amd64 go build -o release/linux/amd64/drone-convert-pathschanged
+GOOS=linux GOARCH=arm64 go build -o release/linux/arm64/drone-convert-pathschanged
